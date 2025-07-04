ecc85fb5c569f50c72de8aaf59144c8240216e6c
Muhammad Afif Faizun <afiffaizun31@gmail.com>
2025-07-04 09:51:17 +0700

    Update Repository

---
+++ src/main/java/com/techmafif/crud/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.techmafif.crud.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.techmafif.crud.model.Product;
+import com.techmafif.crud.service.ProductService;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return ResponseEntity.ok(productService.getAllProduct());
+    }
+    
+}
